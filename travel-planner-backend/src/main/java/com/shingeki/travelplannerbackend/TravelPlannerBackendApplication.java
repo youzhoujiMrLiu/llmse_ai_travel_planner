ba04983017a9337daad56905e64a7303cba907c7
shingeki <1328144075@qq.com>
2025-10-25 17:07:54 +0800

    feat: 后端初始化

---
+++ travel-planner-backend/src/main/java/com/shingeki/travelplannerbackend/TravelPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shingeki.travelplannerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlannerBackendApplication.class, args);
+	}
+
+}
---
+++ travel-planner-backend/src/test/java/com/shingeki/travelplannerbackend/TravelPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shingeki.travelplannerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
